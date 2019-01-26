4ea733d8b96473e497b76f3617bed283f5eeface
albertli354 <xl52@uw.edu>
2019-01-25 17:19:32 -0800

    initial set up

---
+++ lifecounter/lifecounter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Xiang Li on 1/24/19.
+//  Copyright © 2019 Xiang Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func test() {
+        print("Hello World!")
+    }
+
+}
+
